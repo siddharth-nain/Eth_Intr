f751eafaaad577179b0f5dcf4441de14c2e713ea
siddharth-nain <124575984+siddharth-nain@users.noreply.github.com>
2024-05-31 17:21:32 +0530

    Update Start.sol

--- Start.sol
+++ Start.sol
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -51,38 +50,3 @@ contract UserAccount {
         return users[_user].username;
     }
 }
-=======
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Account {
-    mapping(address => uint) public balances;
-    uint public deposits;
-
-    modifier sufficientBalance(uint _amt) {
-        require(balances[msg.sender] >= _amt, "ERROR");
-    _;
-    }
-
-    function deposit() public payable {
-        balances[msg.sender] += msg.value;
-        deposits += msg.value;
-    }
-
-    function withdraw(uint _amt) public sufficientBalance(_amt) {
-        assert(address(this).balance >= _amt);
-        balances[msg.sender] -= _amt;
-        deposits -= _amt;
-        (bool success, ) = msg.sender.call{value: _amt}("");
-        require(success, "Not able to transfer");
-    }
-
-    function transfer(address _recpt, uint _amt) public sufficientBalance(_amt){
-        if (_recpt == address(12)) {
-            revert("Address Not Found");
-        }
-        balances[msg.sender] -= _amt;
-        balances[_recpt] += _amt;
-    }
-}
->>>>>>> 9ddd1e0d5c9b8583ed432b25e76c318e6439be38
